6cbdf0ddf0ae22ca20438d75c755b58ae300dc8f
Cosmin STOICA <cosmin.stoica@zitec.com>
2021-11-21 15:54:00 +0200

    added handlers for multiple node types

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const argv = require('minimist')(process.argv.slice(2), {
     alias: {
         f: ['file'],
-        o: ['output'],
+        d: ['dest'],
+        o: ['omit', 'skip'],
     },
 });
 const pptSlideExtractor = require('./lib/ppt');
 const xml = require('./lib/xml');
+const docx = require('./lib/docx');
+const smartProcessing = require('./lib/nodes');
 
-(async function({ _: [input], output } = argv) {
-    const slideExtractor = pptSlideExtractor(input);
+(async function({ _: [input], dest, skip } = argv) {
+    const slideExtractor = pptSlideExtractor(input, { skip });
 
-    for await (const buffer of slideExtractor) {
-        const slide = buffer.toString();
+    for await (const slide of slideExtractor) {
+        const nodes = xml.parse(slide);
 
-        xml.parse(slide);
+        const sanitizedNodes = smartProcessing.pushNodes(nodes);
+
+        docx.appendNodes(sanitizedNodes);
     }
+
+    return docx.save(dest || input.replace('.pptx', '.docx'));
 })();
